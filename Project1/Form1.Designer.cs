62e9d817d71939f23474ff875fc478baa1e48a2b
alfiinnn <alfinfin007@gmail.com>
2022-10-18 00:23:41 +0700

    Add project files.

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart.Legends.Add(legend1);
+            this.Chart.Location = new System.Drawing.Point(144, 12);
+            this.Chart.Name = "Chart";
+            this.Chart.Size = new System.Drawing.Size(520, 426);
+            this.Chart.TabIndex = 0;
+            this.Chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        TideData tideData;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tideData = new TideData();
+            tideData.Request();
+            Generate();
+        }
+
+        public void Generate()
+        {
+            var objChart = Chart.ChartAreas[0];
+            // ini buat timedate axis x
+            objChart.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+            objChart.AxisX.Minimum = tideData.response.meta.start.ToOADate();
+            objChart.AxisX.Maximum = tideData.response.meta.end.ToOADate();
+
+            //ini buat timedate axis y 
+            objChart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            objChart.AxisY.Minimum = -3;
+            objChart.AxisY.Maximum = 2;
+
+            //clear 
+            Chart.Series.Clear();
+
+            //ini 
+            Series newSeries = new Series("Tide");
+            newSeries.ChartType = SeriesChartType.Column;
+            newSeries.BorderWidth = 2;
+            newSeries.Color = Color.DarkGreen;
+            newSeries.XValueType = ChartValueType.DateTime;
+            Chart.Series.Add(newSeries);
+
+            for (int i = 0; i < tideData.response.data.Count; i++)
+            {
+                Chart.Series[0].Points.AddXY(tideData.response.data[i].time, tideData.response.data[i].height);
+                Refresh();
+            }
+        }
+    }
+}
